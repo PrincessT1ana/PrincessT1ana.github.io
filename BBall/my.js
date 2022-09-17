22c7cd0f4bf522d9bc6dd6c47383538e857f1891
PrincessT1ana <tianaclemmons@gmail.com>
2022-09-16 21:27:09 -0400

    ball

---
+++ BBall/my.js
@@ -0,0 +1,8 @@
+let roomWidth = 600;
+let roomHeight = 500;
+let ballWidth = 50;
+let ballHeight = 50;
+
+let room = document.getElementById("room")
+room.style.width = roomWidth + 'px';
+room.style.height = roomHeight + 'px';
