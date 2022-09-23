adf22a149c797b5f5e1fbe4be6f392fecc536a06
PrincessT1ana <tianaclemmons@gmail.com>
2022-09-22 21:28:22 -0400

    Fixed

--- Animation/Animate.js
+++ Animation/Animate.js
@@ -1,3 +1,29 @@
-const First = "First";
-let imageArray = document.getElementById ('Images');
-let 
+let First = document.getElementById("First")
+
+let imageArray = new Array
+(
+    "/Animation/assets/Asset20.png",
+    "/Animation/assets/Asset21.png",
+    "/Animation/assets/Asset22.png",
+    "/Animation/assets/Asset23.png",
+    "/Animation/assets/Asset24.png",
+    "/Animation/assets/Asset25.png",
+    "/Animation/assets/Asset26.png",
+    "/Animation/assets/Asset27.png" 
+);
+
+let count = 0
+
+function animate() {
+
+    First.src = imageArray [count];
+    count++;
+    if (imageArray.length == count){
+
+        count = 0
+
+    }
+}
+
+setInterval(animate, 100)
+
