09b88784a6c0db13a3c3417cdeedf6703d2537d1
PrincessT1ana <tianaclemmons@gmail.com>
2022-09-23 22:34:44 -0400

    GSAP

---
+++ GSAP/idk.js
@@ -0,0 +1,16 @@
+gsap.from(".chucked", {
+    duration: 3,
+    rotation: 720,
+    ease: "bounce",
+    stagger: .5,
+    x: -500,
+    y: -500
+});
+
+gsap.from(".headline",{
+duration: .5,
+stagger: 0.3,
+opacity: 0, 
+y: 300
+
+});
